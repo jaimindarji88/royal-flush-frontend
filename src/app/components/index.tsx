97e7012e5790b4c30ffdb6e065bfd65b4cbfdabe
Jaimin Darji <jaimin.d.88@gmail.com>
2019-05-02 19:48:40 -0400

    fixed some ui

--- src/app/components/index.tsx
+++ src/app/components/index.tsx
@@ -1,22 +1,22 @@
-import * as _ from 'lodash';
-import * as React from 'react';
-import * as GridLayout from 'react-grid-layout';
+import * as _ from "lodash";
+import * as React from "react";
+import * as GridLayout from "react-grid-layout";
 
-import CardMatrix from './CardMatrix';
-import PokerHistogram from './Histogram';
-import Players from './Players';
-import PokerOdds from './PokerOdds';
+import CardMatrix from "./CardMatrix";
+import PokerHistogram from "./Histogram";
+import Players from "./Players";
+import PokerOdds from "./PokerOdds";
 
 export default function(props: {}) {
   return (
     <GridLayout
-      className="layout"
+      className='layout'
       cols={12}
       rowHeight={30}
       width={window.innerWidth}
     >
       <div
-        key="history"
+        key='history'
         data-grid={{
           x: 0,
           y: 0,
@@ -27,16 +27,16 @@ export default function(props: {}) {
       >
         Hi Story
       </div>
-      <div key="card-matrix" data-grid={{ x: 5, y: 0, w: 5, h: 12 }}>
+      <div key='card-matrix' data-grid={{ x: 5, y: 0, w: 6, h: 12 }}>
         <CardMatrix />
       </div>
-      <div key="players" data-grid={{ x: 10, y: 0, w: 1, h: 5 }}>
+      <div key='players' data-grid={{ x: 10, y: 0, w: 1, h: 5 }}>
         <Players />
       </div>
-      <div key="histogram" data-grid={{ x: 5, y: 12, w: 5, h: 10 }}>
+      <div key='histogram' data-grid={{ x: 5, y: 12, w: 5, h: 10 }}>
         <PokerHistogram />
       </div>
-      <div key="odds" data-grid={{ x: 0, y: 1, w: 4, h: 2 * 2 }}>
+      <div key='odds' data-grid={{ x: 0, y: 1, w: 4, h: 2 * 2 }}>
         <PokerOdds />
       </div>
     </GridLayout>
