ab8423b9f6493d827457100433319e56e5c945fb
Jaimin Darji <jaimin.d.88@gmail.com>
2019-05-14 15:56:17 -0400

    Updates

---
+++ precache-manifest.cd209ac49e4010865459c64491369be9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "41199cb9481e39048d5e",
+    "url": "/royal-flush/static/js/runtime~main.41199cb9.js"
+  },
+  {
+    "revision": "7dfec9efd70fb3fb7afb",
+    "url": "/royal-flush/static/js/main.7dfec9ef.chunk.js"
+  },
+  {
+    "revision": "2eb0c77a6f7eada2d0ea",
+    "url": "/royal-flush/static/js/1.2eb0c77a.chunk.js"
+  },
+  {
+    "revision": "7dfec9efd70fb3fb7afb",
+    "url": "/royal-flush/static/css/main.27b700bb.chunk.css"
+  },
+  {
+    "revision": "2eb0c77a6f7eada2d0ea",
+    "url": "/royal-flush/static/css/1.f1dd841d.chunk.css"
+  },
+  {
+    "revision": "0b2466d213845bbdf519408470d91682",
+    "url": "/royal-flush/index.html"
+  }
+];
